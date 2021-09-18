8bea014f9f0b0b9d36701e0784cdb20e56273295
User <alex.z004@gmail.com>
2021-09-18 17:27:48 +0000

    minor fixes for Ctrl+D

--- lab1/src/org/airng/labfirst/Main.java
+++ lab1/src/org/airng/labfirst/Main.java
@@ -11,25 +11,30 @@ public class Main {
         final int MAX_LENGTH = 17;
         Scanner sc = new Scanner(System.in);
         String command;
-        do {
-            System.out.print(">>> ");
-            command = sc.nextLine();
-            if (command.length() > MAX_LENGTH) {
-                System.out.println("Command is too long");
-                continue;
-            }
-            switch (command) {
-                case "--h" -> System.out.println("""
-                    This is first Java lab.
-                    Available commands:
-                        [number]               calculates its square and cubic root of the square
-                        [number1] [number2]    divides 1st by 2nd number
-                        [string]               sorts the string and prints count of unique chars
-                        --h                    prints this message
-                        q                      quits the program""");
-                case "q" -> System.out.println("Thank you for choosing our product! Good bye.");
-                default -> CommandProcessor.process(command);
-            }
-        } while (!command.equals("q"));
+        try {
+            do {
+                System.out.print(">>> ");
+                command = sc.nextLine();
+                if (command.length() > MAX_LENGTH) {
+                    System.out.println("Command is too long");
+                    continue;
+                }
+                switch (command) {
+                    case "--h" -> System.out.println("""
+                        This is first Java lab.
+                        Available commands:
+                            [number]               calculates its square and cubic root of the square
+                            [number1] [number2]    divides 1st by 2nd number
+                            [string]               sorts the string and prints count of unique chars
+                            --h                    prints this message
+                            q                      quits the program""");
+                    case "q" -> System.out.println("Thank you for choosing our product! Good bye.");
+                    default -> CommandProcessor.process(command);
+                }
+            } while (!command.equals("q"));
+        } catch (java.util.NoSuchElementException e) {
+            // Ctrl+D pressed
+            System.out.println("Good bye.");
+        }
     }
 }
