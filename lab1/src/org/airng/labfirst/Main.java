911ee0906018ce75407ca9b07d8bc5b811bac07f
User <alex.z004@gmail.com>
2021-09-18 17:34:06 +0000

    minor fixes for Ctrl+D

--- lab1/src/org/airng/labfirst/Main.java
+++ lab1/src/org/airng/labfirst/Main.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 
 public class Main {
-    /*
-     *    First lab. The task is available on drive.
-     */
-    public static void main(String[] args) {
-        final int MAX_LENGTH = 17;
-        Scanner sc = new Scanner(System.in);
-        String command;
-        try {
-            do {
-                System.out.print(">>> ");
-                command = sc.nextLine();
-                if (command.length() > MAX_LENGTH) {
-                    System.out.println("Command is too long");
-                    continue;
-                }
-                switch (command) {
-                    case "--h" -> System.out.println("""
+  /*
+   *    First lab. The task is available on drive.
+   */
+  public static void main(String[] args) {
+    final int MAX_LENGTH = 17;
+    Scanner sc = new Scanner(System.in);
+    String command;
+
+    try {
+      do {
+        System.out.print(">>> ");
+        command = sc.nextLine();
+
+        if (command.length() > MAX_LENGTH) {
+          System.out.println("Command is too long");
+          continue;
+        }
+
+        switch (command) {
+          case "--h" -> System.out.println("""
                         This is first Java lab.
                         Available commands:
                             [number]               calculates its square and cubic root of the square
@@ -28,13 +31,14 @@ public class Main {
                             [string]               sorts the string and prints count of unique chars
                             --h                    prints this message
                             q                      quits the program""");
-                    case "q" -> System.out.println("Thank you for choosing our product! Good bye.");
-                    default -> CommandProcessor.process(command);
-                }
-            } while (!command.equals("q"));
-        } catch (java.util.NoSuchElementException e) {
-            // Ctrl+D pressed
-            System.out.println("Good bye.");
+          case "q" -> System.out.println("Thank you for choosing our product! Good bye.");
+          default -> CommandProcessor.process(command);
         }
+
+      } while (!command.equals("q"));
+    } catch (java.util.NoSuchElementException e) {
+      // Ctrl+D pressed
+      System.out.println("Good bye.");
     }
+  }
 }
